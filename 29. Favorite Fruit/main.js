c24edaeb6c5856f52e94fe58144aec0e25f9b884
“[unshaider]” <[unshaider28@gmail.com]>
2024-03-16 03:18:06 +0500

    first commit

--- 29. Favorite Fruit/main.js
+++ 29. Favorite Fruit/main.js
@@ -1,20 +1,20 @@
+"use strict";
 // 29. Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
 // • Make a array of your three favorite fruits and call it favorite_fruits.
 // • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
-
-let favorite_fruits : string[] = ['Apple', 'Mango', 'Pineapple']
-if(favorite_fruits.includes('Banana')){
+let favorite_fruits = ['Apple', 'Mango', 'Pineapple'];
+if (favorite_fruits.includes('Banana')) {
     console.log('I really like Banana!');
 }
-else if(favorite_fruits.includes('Orange')){
+else if (favorite_fruits.includes('Orange')) {
     console.log('I really like Orange!');
 }
-else if(favorite_fruits.includes('Mango')){
+else if (favorite_fruits.includes('Mango')) {
     console.log('I really like Mango!');
 }
-else if(favorite_fruits.includes('Strawberry')){
+else if (favorite_fruits.includes('Strawberry')) {
     console.log('I really like Strawberry!');
 }
-else if(favorite_fruits.includes('Cherry')){
+else if (favorite_fruits.includes('Cherry')) {
     console.log('I really like Cherry!');
 }
--- 30. Hello Admin/main.js
+++ 30. Hello Admin/main.js
@@ -1,13 +1,13 @@
+"use strict";
 // 30. Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
 // • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
 // • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
-
-let usernames : string[] = ['admin', 'ali110', 'babu70', 'aslam420', 'mubashir_123', 'jamal.2'];
+let usernames = ['admin', 'ali110', 'babu70', 'aslam420', 'mubashir_123', 'jamal.2'];
 for (let i = 0; i < usernames.length; i++) {
-    if(usernames[i] === 'admin'){
+    if (usernames[i] === 'admin') {
         console.log(`Hello ${usernames[0]}, would you like to see a status report?`);
-    }else{
+    }
+    else {
         console.log(`Hello ${usernames[i]}, thank you for logging in again.`);
     }
 }
-
