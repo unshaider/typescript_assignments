80dbaa63c4885031f0fa26e33387ea7a0f1c783a
“[unshaider]” <[unshaider28@gmail.com]>
2024-03-16 07:27:13 +0500

    first commit

--- 31. No Users/main.js
+++ 31. No Users/main.js
@@ -1,3 +1,10 @@
+"use strict";
 // 31. No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
 // • If the list is empty, print the message We need to find some users!
 // • Remove all of the usernames from your array, and make sure the correct message is printed.
+let usernames = [];
+if (usernames.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+}
--- 36. T-Shirt/main.js
+++ 36. T-Shirt/main.js
@@ -1,6 +1,6 @@
+"use strict";
 // 36. T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
-
-function make_shirt(size : string, text : string) {
+function make_shirt(size, text) {
     console.log(`My shirt size is ${size}, and ${text}`);
 }
-make_shirt('small', 'It is my favorite shirt.')
+make_shirt('small', 'It is my favorite shirt.');
